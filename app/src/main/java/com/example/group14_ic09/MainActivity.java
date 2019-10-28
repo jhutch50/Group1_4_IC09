27e3382c41e7150b1839b12a6c46febab43b8942
ankit <ankitkejriwaal@gmail.com>
2019-10-28 19:58:56 -0400

    modify xml for login screen

--- app/src/main/java/com/example/group14_ic09/MainActivity.java
+++ app/src/main/java/com/example/group14_ic09/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.group14_ic09;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int SIGN_UP_REQ = 123;
+    EditText editTextemail;
+    EditText editTextpassword;
+    Button buttonlogin;
+    Button buttonsignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonsignup = findViewById(R.id.buttonsignup);
+        buttonlogin = findViewById(R.id.buttonlogin);
+        buttonsignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,SignUp.class);
+                startActivityForResult(intent,SIGN_UP_REQ);
+            }
+        });
     }
 }
