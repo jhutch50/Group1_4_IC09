56f979d6a8b09b979eff63ef24d148f1ee504737
Joshua Hutcheson <jhutch50@uncc.edu>
2019-10-28 19:26:52 -0400

    Adding All Activities

---
+++ app/src/main/java/com/example/group14_ic09/DisplayMail.java
@@ -0,0 +1,14 @@
+package com.example.group14_ic09;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DisplayMail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_mail);
+    }
+}
