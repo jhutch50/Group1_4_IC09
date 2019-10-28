36683eebdc4916ac68322a83617e908644620e14
Joshua Hutcheson <jhutch50@uncc.edu>
2019-10-28 19:45:15 -0400

    Updated dependencies to http3

--- app/src/main/java/com/example/group14_ic09/SignUp.java
+++ app/src/main/java/com/example/group14_ic09/SignUp.java
@@ -1,8 +1,19 @@
 package com.example.group14_ic09;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+
+
+import java.io.IOException;
+
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 
 public class SignUp extends AppCompatActivity {
 
@@ -10,5 +21,26 @@ public class SignUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+
     }
+
+    private final OkHttpClient client = new OkHttpClient();
+
+    public void run() throws Exception {
+        RequestBody formBody = new FormBody.Builder()
+                .add("search", "Jurassic Park")
+                .build();
+        Request request = new Request.Builder()
+                .url("https://en.wikipedia.org/w/index.php")
+                .post(formBody)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+            System.out.println(response.body().string());
+        }
+    }
+
 }
